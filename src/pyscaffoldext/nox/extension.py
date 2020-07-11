d61d1c72be93a97e9acd6bc15f000ed219794a8a
Sarthak Jariwala <jariwala@uw.edu>
2020-07-11 00:15:46 -0700

    add nox extension- working with templates, modify gitignore

--- src/pyscaffoldext/nox/extension.py
+++ src/pyscaffoldext/nox/extension.py
@@ -1,15 +1,44 @@
 # -*- coding: utf-8 -*-
-from pyscaffold.api import Extension
+"""Implementation to add noxfile.py to
+PyScaffold project
+"""
+from pathlib import PurePath
+
+from pyscaffold.api import Extension, helpers
+
+from .templates import noxfile
 
 
 class Nox(Extension):
     """
-    This class serves as the skeleton for your
-    new PyScaffold Extension. Refer to the official
-    documentation to discover how to implement a
-    PyScaffold extension.
-    (https://pyscaffold.org/en/latest/extensions.html)
+    Generate Nox configuration file for PyScaffold
     """
 
     def activate(self, actions):
+        """Activate extension
+
+        Args:
+            actions (list): list of actions to perform
+
+        Returns:
+            list: updated list of actions
+        """
+        actions = self.register(actions, self.add_noxfile, after="define_structure")
         return actions
+
+    def add_noxfile(self, struct, opts):
+        """Add a noxfile.py file
+
+        Args:
+            struct (dict): project representation as (possibly) nested
+                :obj:`dict`.
+            opts (dict): given options, see :obj:`create_project` for
+                an extensive list.
+
+        Returns:
+            struct, opts: updated project representation and options
+        """
+        files = PurePath(opts["project"], "noxfile.py")
+        content = noxfile(opts)
+        struct = helpers.ensure(struct, files, content, helpers.NO_OVERWRITE)
+        return struct, opts
---
+++ src/pyscaffoldext/nox/templates/__init__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Templates for all files this extension provides
+"""
+import os
+import string
+from pkg_resources import resource_string
+
+
+def get_template(name):
+    """Retrieve the template by name
+    Args:
+        name: name of template
+    Returns:
+        :obj:`string.Template`: template
+    """
+    file_name = "{name}.template".format(name=name)
+    data = resource_string(__name__, file_name)
+    # we assure that line endings are converted to '\n' for all OS
+    data = data.decode(encoding="utf-8").replace(os.linesep, '\n')
+    return string.Template(data)
+
+
+def noxfile(opts):
+    """Template for noxfile.py
+
+    Args:
+        opts (dict): Missing parameters
+    Returns:
+        str (str): File content as string
+    """
+    template = get_template("noxfile")
+    return template.substitute(opts)
